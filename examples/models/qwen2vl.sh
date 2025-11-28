ae935b848c3112422c7bda1a2853f6d35c146527
wangwensong <3205775977@qq.com>
2025-11-28 11:04:06 +0800

    rope

--- examples/models/qwen2vl.sh
+++ examples/models/qwen2vl.sh
@@ -6,7 +6,7 @@ pip3 install qwen_vl_utils
 # PATCH_SELECTION_METHOD: 'v1' | 'v2' | 'budget' (default: 'budget')
 # ALPHA: float (for 'v2' it's a threshold multiplier; for 'budget' it's fraction 0-1)
 PATCH_SELECTION_METHOD=${PATCH_SELECTION_METHOD:-budget}
-ALPHA=${ALPHA:-1}
+ALPHA=${ALPHA:-0.0625}
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PROJECT_ROOT="$(cd "$SCRIPT_DIR/../.." && pwd)"
@@ -14,8 +14,8 @@ TRANSFORMERS_SRC="${PROJECT_ROOT}/Qwen2-VL/transformers/src"
 
 export PYTHONPATH="${TRANSFORMERS_SRC}:${PYTHONPATH}"
 
-CUDA_VISIBLE_DEVICES=4,5,6,7 \
-accelerate launch --num_processes=1 --main_process_port=12345 -m lmms_eval \
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
+accelerate launch --num_processes=8 --main_process_port=12345 -m lmms_eval \
     --model qwen2_vl \
     --model_args=pretrained=Qwen/Qwen2-VL-7B-Instruct,max_pixels=2359296,patch_selection_method=${PATCH_SELECTION_METHOD},alpha=${ALPHA} \
     --tasks mmstar  \
